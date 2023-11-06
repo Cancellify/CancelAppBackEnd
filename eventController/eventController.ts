37cebf3faa0dc17f221fb1c6a3787d7abf85f1bf
William Brammer <williamedwardbrammer@gmail.com>
2023-11-06 17:42:50 +0900

    feat: iplement modes to create attendance

--- eventController/eventController.ts
+++ eventController/eventController.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import { createNewEvent } from '../eventModel/eventModel';
+import { getUser } from '../userModel/userModel';
 
 
 
 async function createEvent(req: Request, res: Response) {
     try {
-      const { eventName, eventDescription, date} = req.body
+      const { eventName, eventDescription, date, invitees} = req.body
+
+      console.log(invitees);
+      
 
       const sentEventData = {
         event_name: eventName,
@@ -13,11 +17,23 @@ async function createEvent(req: Request, res: Response) {
         Date: date,
       }
 
-      await createNewEvent(sentEventData)
+      console.log(sentEventData)
+
+      const responseID = await createNewEvent(sentEventData);
+
+      let userIdArray = [];
+      let userEmailArray = [];
+
+      for(let i = 0; i < invitees.length; i++ ){
+        let resultId = await getUser(invitees[i]);
+        userIdArray.push(resultId?.id);
+        userEmailArray.push(resultId?.email)
+      }
+
 
       res.status(201).send("Event created");
     } catch (error: any) {
-      res.status(409).send(`Failed to create account: ${error.message}`);
+      res.status(409).send(`Failed to create event: ${error.message}`);
     }
   }
 
--- eventModel/eventModel.ts
+++ eventModel/eventModel.ts
@@ -3,8 +3,12 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function createNewEvent(newEvent: any) {
-    await prisma.event.create({data : newEvent})
-
+    const data = await prisma.event.create({data : newEvent})
+return data.id
 }
+
+// async function createAttendance(event) {
+    
+// }
 
 export {createNewEvent}
